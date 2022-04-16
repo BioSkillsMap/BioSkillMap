96a6805d0b9ebc65f7731076a5e6f62572be98e2
Gabriel <75862004+Gabriel1961@users.noreply.github.com>
2022-04-16 13:50:57 +0300

    auth update (merge sa te autentifici)

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { IoPersonCircleSharp } from "react-icons/io5";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  database: process.env.DATABASE_URL,
+  adapter: PrismaAdapter(prisma),
+  
+});
